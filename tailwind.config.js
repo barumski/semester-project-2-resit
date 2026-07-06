9aedcee13d110271072eb5165155f8a9388aec8c
barumski <joebae00157@stud.noroff.no>
2026-07-06 23:34:19 +0200

    Initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./account/**/*.html",
+    "./pet/**/*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
